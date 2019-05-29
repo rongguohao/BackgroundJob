fb7d69629c5f45552b1027a9fb710c7283286ecc
rongguohao <rongguohao@vip56.cn>
2019-05-29 10:53:24 +0800

    1

---
+++ Hao.Hf.DyService/DyService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Hf.DyService
+{
+    public class DyService:IDyService
+    {
+    }
+}
---
+++ Hao.Hf.DyService/Interface/IDyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Hf.DyService
+{
+    public interface IDyService
+    {
+
+    }
+}
