406ebcfcc61f721f62153e6dd98687f958373cdf
rongguohao <rongguohao@vip56.cn>
2019-06-20 17:27:20 +0800

    1

---
+++ Hao.Hf.DyService/Country.cs
@@ -0,0 +1,13 @@
+﻿using Hao.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Hf.DyService
+{
+    public enum AreaType
+    {
+        [HDescription("中国大陆")]
+        China
+    }
+}
