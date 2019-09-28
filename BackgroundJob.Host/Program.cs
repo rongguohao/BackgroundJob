2975149130e5048ae3cbfcb50fef42edf77340d0
rongguohao <843468011@qq.com>
2019-09-28 11:12:06 +0800

    1

---
+++ BackgroundJob.Host/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundJob.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000") //hangfire看板只能打开5000端口
+                .Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)  //默认的builder 读取配置文件
+                .UseStartup<Startup>();
+    }
+}
---
+++ MovieProgram/Program.cs
@@ -0,0 +1,54 @@
+﻿using Hao.Hf.DyService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace MovieProgram
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false) // 在运行时修改强类型配置，无需设置reloadOnChange:true,默认就为true,只需要使用IOptionsSnapshot接口,IOptions<> 生命周期为Singleton,IOptionsSnapshot<> 生命周期为Scope
+                .AddJsonFile($"appsettings.Development.json", optional: true)  //没有的话 默认读取appsettings.json
+                .AddEnvironmentVariables()
+                .Build();
+
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<IConfiguration>(configuration);
+            services.AddHttpClient("dy", a => { a.Timeout = TimeSpan.FromMinutes(3); })
+                        .AddPolicyHandler(Policy<HttpResponseMessage>
+                        .Handle<SocketException>()
+                        .Or<IOException>()
+                        .Or<HttpRequestException>()
+                        .WaitAndRetryForeverAsync(t => TimeSpan.FromSeconds(5), (ex, ts) =>
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("重试" + ts);
+                        }))
+                        .ConfigureHttpMessageHandlerBuilder((c) =>
+                        new HttpClientHandler()
+                        {
+                            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+                        });
+
+            services.AddTransient<IDyService, DyService>();
+            services.AddTransient<IHttpHelper, HttpHelper>();
+
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            var dyService = serviceProvider.GetService<IDyService>();
+
+            await dyService.PullMovieJustOnce();
+        }
+    }
+}
